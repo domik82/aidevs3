062e208d704e997544076fae542e8ab4a2ba7ac5
Dominik Jeziorski <d.jeziorski@gmail.com>
2024-11-16 19:25:11 +0100

    S02E03 generate

---
+++ tasks/S02E03/S02E03.py
@@ -0,0 +1,30 @@
+import os
+
+from dotenv import load_dotenv
+from icecream import ic
+from loguru import logger
+
+from common.aidevs3_taskhandler import TaskHandler
+
+load_dotenv()
+AI_DEVS_CENTRALA_ADDRESS = os.getenv("AI_DEVS_CENTRALA_ADDRESS")
+AI_DEVS_CENTRALA_TOKEN = os.getenv("AI_DEVS_CENTRALA_TOKEN")
+
+
+def main():
+    try:
+        task_name = "robotid"
+        result = "https:/abc/file.png"
+        handler = TaskHandler(AI_DEVS_CENTRALA_ADDRESS, AI_DEVS_CENTRALA_TOKEN)
+        answer_response = handler.post_answer(task_name, result)
+        logger.info(f"{task_name} answer Response: {answer_response.response_json}")
+
+        assert answer_response.code == 0, "We have proper response code"
+        logger.info(f"{answer_response.message}")
+
+    except Exception as e:
+        ic(f"An error occurred: {str(e)}")
+
+
+if __name__ == "__main__":
+    main()
